588262afc319f430c76dd73ef908653bbed3f129
pipinstall89 <nclarkrva@gmail.com>
2021-11-09 19:55:37 -0500

    corrected welcome message

--- web_app.go
+++ web_app.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "You sonna b'gun, I'm in", request.URL.Path[1:])
+	fmt.Fprintf(writer, "Welcome one and all", request.URL.Path[1:])
 }
 
 func main() {
